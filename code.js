0ced7151f4ff5f5e2d3d5181fe83c1050cdd1da1
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-05-09 14:30:02 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function Tailrecursion(n) {
     return [0];
   if (n == 1)
     return [0,1];
-  var array = fib(n-1);
+  var array = Tailrecursion(n-1);
   array.push(array[array.length - 2]+array[array.length - 1]);
   return array;
 }

94ce8aa011ce707c9a7e5ca8d3f1084ffe4a05dc
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-05-09 14:49:46 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,16 @@ function fib(n) {
     return [0];
   if (n == 1)
     return [0,1];
-  var array = fib(n-1);
-  array.push(array[array.length - 2]+array[array.length - 1]);
-  return array;
+
+  return tailRecursive(2, 0, 1, [0, 1], n);
+  //var array = fib(n-1);
+  //array.push(array[array.length - 2]+array[array.length - 1]);
+  //return array;
+}
+
+function tailRecursive(iter, two, one, current, n) {
+  if (iter >= n) {
+    return current;
+  }
+  return tailRecursive((iter + 1), one, (two + one), current.concat(two + one));
 }
